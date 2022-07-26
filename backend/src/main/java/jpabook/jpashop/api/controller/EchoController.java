41490ef82d120814b17e6870b8324f866ec870f7
ParkSeolDev <parkseol.dev@gmail.com>
2022-07-26 11:45:13 +0900

    FEAT : 데이터 잘림 현상 해결

--- backend/src/main/java/jpabook/jpashop/api/controller/EchoController.java
+++ backend/src/main/java/jpabook/jpashop/api/controller/EchoController.java
@@ -1,64 +1,36 @@
 package jpabook.jpashop.api.controller;
-
-import jpabook.jpashop.api.service.DataService;
-import jpabook.jpashop.db.dto.DataDTO;
 import jpabook.jpashop.socket.Connection;
 import lombok.RequiredArgsConstructor;
-import java.util.List;
 
 //0033A99520000000000000000{0:0428,1:1}
 
 @TcpController
 @RequiredArgsConstructor
 public class EchoController {
-    private final DataService dataService;
-    private final int numByte = 4;
-    private final Double bufferSize = 20.0;
-    private int count = 10000;
-    private Double skip = 1.0 + Math.ceil((count - (bufferSize - numByte)) / bufferSize);
-    private StringBuilder text = new StringBuilder();
+
+    private StringBuilder header = new StringBuilder();
+    private StringBuilder body = new StringBuilder();
 
     public void receiveData(Connection connection, byte[] data) {
 
         String s = new String(data);
         connection.send(s.toUpperCase().getBytes());
+        int i = 0;
+        while(i < s.length()){
 
-        if (count == 10000) {
-            count = Integer.parseInt(s.substring(0, numByte));
-//            skip = 1.0 + Math.floor((count - numByte) / bufferSize);
-            skip = 1.0 + Math.ceil((count - (bufferSize - numByte)) / bufferSize);
-            String str = s.substring(numByte);
-            // DataDTO data1 = new DataDTO();
-            // data1.setSplitData(str);
-            // dataService.createData(data1);
-            // text.append(data1.getSplitData());
-            text.append(str);
-        } else {
-            // DataDTO data1 = new DataDTO();
-            // data1.setSplitData(s);
-            // dataService.createData(data1);
-            // text.append(data1.getSplitData());
-            text.append(s);
-        }
-//        fixSplitData();
-        skip--;
-
-        if (skip == 0) {
-            System.out.println(text.toString());
-            text = new StringBuilder();
-            count = 10000;
-            skip = 1.0 + Math.ceil((count - (bufferSize - numByte)) / bufferSize);
-            // dataService.setIsPrinted();
+            if(header.length() < 4){
+                header.append(s.charAt(i));
+            }else if(header.length() == 4 & body.length() < Integer.parseInt(header.toString())){
+                body.append(s.charAt(i));
+            }
+            if (header.length() == 4 & body.length() == Integer.parseInt(header.toString())){
+                System.out.println(body);
+                header = new StringBuilder();
+                body = new StringBuilder();
+            }
+            i++;
         }
     }
-
-    public void fixSplitData() {
-        List<DataDTO> list = dataService.getSplitData();
-        for (DataDTO data : list) {
-            text.append(data.getSplitData());
-        }
-    }
-
 
     public void connect(Connection connection) {
         System.out.println("New connection " + connection.getAddress().getCanonicalHostName());
