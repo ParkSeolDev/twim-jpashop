016b54e72bd81bd95ac8dab0fb99fe9fdb3a4477
ParkSeolDev <parkseol.dev@gmail.com>
2022-07-28 10:26:59 +0900

    CHORE ; 데이터 관련 서비스, 레파지토리, 엔티티, DTO 삭제

--- backend/src/main/java/jpabook/jpashop/db/repository/DataRepository.java
+++
@@ -1,7 +0,0 @@
-package jpabook.jpashop.db.repository;
-
-import jpabook.jpashop.db.entity.Data;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DataRepository extends JpaRepository<Data, Long> {
-}
