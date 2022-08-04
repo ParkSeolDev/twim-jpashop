68fd31d40bb2c40c329128300e9ad8e9b4345992
ParkSeolDev <parkseol.dev@gmail.com>
2022-08-04 16:13:06 +0900

    CHORE : 수정

--- backend/src/main/java/jpabook/jpashop/api/controller/EchoController.java
+++ backend/src/main/java/jpabook/jpashop/api/controller/EchoController.java
@@ -11,13 +11,34 @@ public class EchoController {
     private StringBuilder header = new StringBuilder();
     private StringBuilder body = new StringBuilder();
 
+    // public void receiveData(Connection connection, byte[] data) {
+
+    //     String s = new String(data);
+    //     connection.send(s.toUpperCase().getBytes());
+    //     int i = 0;
+    //     while(i < s.length()){
+
+    //         if(header.length() < 4){
+    //             header.append(s.charAt(i));
+    //         }else if(header.length() == 4 & body.length() < Integer.parseInt(header.toString())){
+    //             body.append(s.charAt(i));
+    //         }
+    //         if (header.length() == 4 & body.length() == Integer.parseInt(header.toString())){
+    //             System.out.println(body);
+    //             header = new StringBuilder();
+    //             body = new StringBuilder();
+    //         }
+    //         i++;
+    //     }
+    // }
+
     public void receiveData(Connection connection, byte[] data) {
 
         String s = new String(data);
         connection.send(s.toUpperCase().getBytes());
         int i = 0;
         while(i < s.length()){
-
+            
             if(header.length() < 4){
                 header.append(s.charAt(i));
             }else if(header.length() == 4 & body.length() < Integer.parseInt(header.toString())){
